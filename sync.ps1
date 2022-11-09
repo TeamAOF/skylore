0836613ed5c0a06bcefc367883c4811d7da5d5d1
_Robertas <66907345+RobertasJ@users.noreply.github.com>
2022-11-09 20:06:38 +0100

    added info for impatient people
    some people might think the modpack isnt installing so i added the please wait line after each step.

--- sync.ps1
+++ sync.ps1
@@ -13,12 +13,20 @@ $data = Get-Content -raw ../scriptData.json | ConvertFrom-Json
 # for some reason if they match it outputs false, its so ass backwards
 if(Compare-Object $res.pushed_at $data.lastUpdated) {
     Write-Output "`n`nNew version detected, updating!`n`n"
-
+    Start-Sleep 0.5
+    write-Output "Downloading modpack, please wait.`n`n"
     Invoke-WebRequest -Uri "https://github.com/TeamAOF/skylore/archive/refs/heads/main.zip " -OutFile "./skylore-main.zip" 
+    write-Output "Unzipping modpack, please wait."
+    Start-Sleep 1
     Expand-Archive skylore-main.zip
+    write-Output "Copying modpack, please wait."
+    Start-Sleep 1
     xcopy /s /y skylore-main\skylore-main\*.* .
     Remove-Item -Force -Recurse skylore-main
     Remove-Item -Force "./skylore-main.zip"
+
+    write-Output "Downloading mods, please wait."
+    Start-Sleep 1
     java -jar InstanceSync.jar
 
     $data.lastUpdated = $res.pushed_at
@@ -26,7 +34,5 @@ if(Compare-Object $res.pushed_at $data.lastUpdated) {
 }
 else {
     Write-Output "`n`nUp to date!`n`n"
+    Start-Sleep 1
 }
-
-
-
