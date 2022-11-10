fd4688678e3a279e47151a2342a0d42c17181ab8
_Robertas <robertasj8@gmail.com>
2022-11-10 22:19:18 +0100

    ability to change which branch to download from with - branch
    ability to change which branch to download from with - branch
    
    you can now add the -branch indev or any other branch name to automatically download from it instead of the main branch

--- sync.ps1
+++ sync.ps1
@@ -1,38 +1,46 @@
-$res = Invoke-RestMethod -Uri https://api.github.com/repos/TeamAOF/skylore
+# params and constants
+param (
+    $branch = "main"
+)
+$defaultConfig = ConvertFrom-Json -InputObject '{
+    "lastUpdated": "never"
+}'
 
-if(Test-Path -Path ../scriptData.json -PathType Leaf) {
-}
-else {
-    New-Item "../scriptData.json"
-    '{
-        "lastUpdated": "never"
-    }' | Out-File "../scriptData.json"
+
+# config tests
+if (!(Test-Path -Path ../scriptData.json -PathType Leaf)) {
+    ConvertTo-Json -InputObject $defaultConfig -depth 5 | Out-File "../scriptData.json"
 }
 $data = Get-Content -raw ../scriptData.json | ConvertFrom-Json
 
-# for some reason if they match it outputs false, its so ass backwards
-if(Compare-Object $res.pushed_at $data.lastUpdated) {
-    Write-Output "`n`nNew version detected, updating!`n`n"
-    Start-Sleep 0.5
-    write-Output "Downloading modpack, please wait.`n`n"
-    Invoke-WebRequest -Uri "https://github.com/TeamAOF/skylore/archive/refs/heads/main.zip " -OutFile "./skylore-main.zip" 
-    write-Output "Unzipping modpack, please wait."
-    Start-Sleep 1
-    Expand-Archive skylore-main.zip
-    write-Output "Copying modpack, please wait."
-    Start-Sleep 1
-    xcopy /s /y skylore-main\skylore-main\*.* .
-    Remove-Item -Force -Recurse skylore-main
-    Remove-Item -Force "./skylore-main.zip"
+# code
+$res = Invoke-RestMethod -Uri https://api.github.com/repos/TeamAOF/skylore
 
-    write-Output "Downloading mods, please wait."
+# for some reason if they match it outputs false, its so ass backwards
+if (Compare-Object $res.pushed_at $data.lastUpdated) {
+    "`n`nNew version detected, updating!`n`n"
+    Start-Sleep 0.5
+    "`n`nDownloading modpack.`n`n"
+    $uri = "https://github.com/TeamAOF/skylore/archive/refs/heads/" + $branch + ".zip"
+    Invoke-WebRequest -Uri $uri -OutFile "./skylore.zip" 
+    "`n`nUnzipping modpack.`n`n"
+    Start-Sleep 3
+    Expand-Archive skylore.zip
+    "`n`nCopying modpack.`n`n"
+    Start-Sleep 3
+    xcopy /s /y skylore\skylore-main\*.* .
+    Remove-Item -Force -Recurse skylore
+    Remove-Item -Force "./skylore.zip"
+
+    "`n`nDownloading mods, please wait.`n`n"
     Start-Sleep 1
     java -jar InstanceSync.jar
 
     $data.lastUpdated = $res.pushed_at
     $data | ConvertTo-Json -depth 5 | Out-File "../scriptData.json"
+    "`n`nDone!`n`n"
 }
 else {
-    Write-Output "`n`nUp to date!`n`n"
+    "`n`nUp to date!`n`n"
     Start-Sleep 1
 }
