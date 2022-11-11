491d44ab2eebce54ef7740f4a88e1176a2128108
_Robertas <robertasj8@gmail.com>
2022-11-11 20:37:30 +0100

    make newlines work

--- sync.sh
+++ sync.sh
@@ -28,24 +28,24 @@ pushed_at=`curl https://api.github.com/repos/TeamAOF/skylore | jq '.pushed_at'`
 
 if [ "$lastUpdated" == "$pushed_at" ]; then
     echo true
-    echo "\n\nNew version detected, updating!\n\n"
+    echo "\n \n New version detected, updating! \n \n"
     sleep 0.5
-    echo "\n\nDownloading modpack.\n\n"
+    echo "\n \n Downloading modpack. \n \n"
     curl "https://github.com/TeamAOF/skylore/archive/refs/heads/${branch}.zip" --output ./skylore.zip
-    echo "\n\nUnzipping modpack.\n\n"
+    echo "\n \n Unzipping modpack. \n \n"
     sleep 3
     unzip ./skylore.zip
-    echo "\n\nCopying modpack.\n\n"
+    echo "\n \n Copying modpack. \n \n"
     sleep 3
     cp ./skylore/skylore-$branch/* .
-    echo "\n\nDownloading mods, please wait.\n\n"
+    echo "\n \n Downloading mods, please wait. \n \n"
     sleep 3
     java -jar InstanceSync.jar
 
     jq ".lastUpdated |= ${pushed_at}" ./scriptData.json
-    echo "\n\nDone!\n\n"
+    echo "\n \nDone! \n \n"
 else
     echo false
-    echo "\n\nUp to date!\n\n"
+    echo "\n \nUp to date! \n \n"
     sleep 1
 fi
