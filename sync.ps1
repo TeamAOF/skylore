8855df7de6bb50b8142ab6f4015aca1ce133be5b
_Robertas <robertasj8@gmail.com>
2022-11-17 18:12:28 +0100

    uses only config now, no parameters needed.
    this will make it easier to ship a dev built to anyone with this script as the provider will only need to supply the config, the user can always edit the file unless the git repo theyre using didnt put the config files into the .gitignore file

--- sync.ps1
+++ sync.ps1
@@ -1,11 +1,11 @@
 # params and constants
-param (
-    $owner = "TeamAOF",
-    $repo = "skylore",
-    $branch = "indev"
-)
+. .\config.ps1
+# example config 
 
-
+# # used for setting the rigth url to download from.
+# $owner = "TeamAOF" # creator of the git repository.
+# $repo = "skylore" # name of the git repository.
+# $branch = "indev" # branch of the git repository. for examle main or master.
 
 $repoExists = git rev-parse --is-inside-work-tree
 $url = "https://github.com/" + $owner + "/" + $repo + ".git"
