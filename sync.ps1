124c5e30406dae60997f06933d2e5b806cf8be01
_Robertas <robertasj8@gmail.com>
2022-11-17 19:53:55 +0100

    how did i fuck this up lmao

---
+++ sync.ps1
@@ -0,0 +1,33 @@
+# params and constants
+. .\config.ps1
+# example config 
+
+# # used for setting the rigth url to download from.
+# $owner = "TeamAOF" # creator of the git repository.
+# $repo = "skylore" # name of the git repository.
+# $branch = "indev" # branch of the git repository. for examle main or master.
+
+$repoExists = git rev-parse --is-inside-work-tree
+$url = "https://github.com/" + $owner + "/" + $repo + ".git"
+
+# code
+
+if ($repoExists) {
+    git switch $branch
+    git reset --hard
+    git pull
+    java -jar InstanceSync.jar
+}
+else {
+    git clone $url modpack
+    Move-Item modpack\* .
+    Move-Item modpack\.* .
+    Remove-Item modpack -Recurse
+    git reset --hard
+    git pull
+    git switch $branch
+    java -jar InstanceSync.jar
+}
+if (Test-Path -Path ./server.lock) {
+    . .\server.lock
+}
