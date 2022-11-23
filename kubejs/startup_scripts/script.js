4afb669b8486ff8f50c5e372ac3e5fb3ba412d64
_Robertas <robertasj8@gmail.com>
2022-11-23 15:27:26 +0100

    crushed deepslate now has gravity

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -8,10 +8,11 @@ onEvent('item.registry', event => {
 })
 
 onEvent('block.registry', event => {
-	event.create('crushed_deepslate')
+	event.create('crushed_deepslate', 'falling')
 	.material('dirt')
   .hardness(0.5)
   .displayName('Crushed Deepslate') // No longer required in 1.18.2+
   .tagBlock('minecraft:mineable/shovel') // Make it mine faster using a shovel in 1.18.2+
+  .dustColor(color.GRAY)
   
 })
