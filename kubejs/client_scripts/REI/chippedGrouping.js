b82c2a7dede23bac146d2bad81742d88d5d7453e
_Robertas <robertasj8@gmail.com>
2022-12-06 22:01:44 +0100

    copy pasted some code

--- kubejs/client_scripts/REI/chippedGrouping.js
+++ kubejs/client_scripts/REI/chippedGrouping.js
@@ -1,30 +1,13 @@
-"use strict"
-// functions 
-function blockIdToBlockName(tags) {
-    return tags.map((id) => {
-        let wordArray = id.split("_")
-        wordArray[0] = wordArray[0].slice(wordArray[0].search(/[:]/) + 1)
-        let upperCaseWordArray = wordArray.map((word) => {return word[0].toUpperCase() + word.slice(1)})
-        let upperCaseWord = ""
-        upperCaseWordArray.forEach(word => {
-            upperCaseWord += word + " "
-        })
-        return upperCaseWord.trim()
-    })
-}
-
-// code
-
-const tags = [
-    "chipped:black_stained_glass",
-    "chipped:lime_terracotta"
-]
-
-const blockNames = blockIdToBlockName(tags)
-
-onEvent("rei.group", (event) => {
-    for (let i = 0; i < tags.length; i++) {
-        let tagNoModId = tags[i].slice(tags[i].search(/[:]/) + 1)
-        event.groupItemsByTag(`chipped:rei_groups/${tagNoModId}`, blockNames[i], tags[i]);
-    }
+onEvent("rei.group", event => {
+    Ingredient.of("@chipped").stacks.stream()
+    .flatMap(stack => stack.tags.stream())
+    .distinct() // Exclude duplicate tag entries
+    .filter(tag => tag.namespace == "chipped")
+    .forEach(tag => {
+      // Human-readable title case, e.g white_wool -> White Wool
+      const name = tag.path
+        .replace(/^(\w)/, c => c.toUpperCase())
+        .replace(/_(\w)/g, (_, c) => " " + c.toUpperCase());
+      event.groupItemsByTag(`${tag.namespace}:rei_groups/${tag.path}`, name, tag);
+    });
 })
