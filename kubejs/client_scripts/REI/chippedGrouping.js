19ee7b9751076fee2f74f1c945dc40b390ccd295
_Robertas <robertasj8@gmail.com>
2022-12-06 21:40:50 +0100

    working script

--- kubejs/client_scripts/REI/chippedGrouping.js
+++ kubejs/client_scripts/REI/chippedGrouping.js
@@ -1,10 +1,11 @@
+"use strict"
 // functions 
 function blockIdToBlockName(tags) {
     return tags.map((id) => {
-        wordArray = id.split("_")
+        let wordArray = id.split("_")
         wordArray[0] = wordArray[0].slice(wordArray[0].search(/[:]/) + 1)
-        upperCaseWordArray = wordArray.map((word) => {return word[0].toUpperCase() + word.slice(1)})
-        var upperCaseWord = ""
+        let upperCaseWordArray = wordArray.map((word) => {return word[0].toUpperCase() + word.slice(1)})
+        let upperCaseWord = ""
         upperCaseWordArray.forEach(word => {
             upperCaseWord += word + " "
         })
@@ -22,14 +23,8 @@ const tags = [
 const blockNames = blockIdToBlockName(tags)
 
 onEvent("rei.group", (event) => {
-
-    
-    
-
-
-    tags.forEach((tag, index) => {
-        tagNoModId = tag.slice(tag.search(/[:]/) + 1)
-        event.groupItemsByTag(`chipped:rei_groups/${tagNoModId}`, blockNames[index], tag);
-    })
-
+    for (let i = 0; i < tags.length; i++) {
+        let tagNoModId = tags[i].slice(tags[i].search(/[:]/) + 1)
+        event.groupItemsByTag(`chipped:rei_groups/${tagNoModId}`, blockNames[i], tags[i]);
+    }
 })
