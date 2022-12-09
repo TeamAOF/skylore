b62318b7656914c8f5409c20b9c612d0a92a15f4
_Robertas <robertasj8@gmail.com>
2022-12-10 00:36:12 +0100

    is this the issue
    This reverts commit 6fb8305ffe0e660f455e139cc5a8d8a7fb65d871.

--- kubejs/client_scripts/REI/itemTagGrouping.js
+++ kubejs/client_scripts/REI/itemTagGrouping.js
@@ -2,20 +2,6 @@
 // code
 
 const tags = [
-    "botanypots:basic_botany_pots",
-    "botanypots:hopper_botany_pots",
-    "another_furniture:chairs",
-    "another_furniture:shelves",
-    "another_furniture:tables",
-    "another_furniture:stools",
-    "another_furniture:shutters",
-    "another_furniture:curtains",
-    "another_furniture:benches",
-    "another_furniture:drawers",
-    "another_furniture:chairs",
-    "another_furniture:lamps",
-    "another_furniture:sofas",
-    "another_furniture:tall_stools"
 ]
 
 onEvent("rei.group", (rei) => {
