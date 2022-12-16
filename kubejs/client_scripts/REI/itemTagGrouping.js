ff9579b221e77555f0a495e45eec76dee8523c72
_Robertas <robertasj8@gmail.com>
2022-12-16 20:34:22 +0100

    should be working script for grouping items in rei

--- kubejs/client_scripts/REI/itemTagGrouping.js
+++ kubejs/client_scripts/REI/itemTagGrouping.js
@@ -1,10 +1,28 @@
 "use strict"
+// functions 
+function blockIdToBlockName(tags) {
+    return tags.map((id) => {
+        let wordArray = id.split("_")
+        wordArray[0] = wordArray[0].slice(wordArray[0].search(/[:]/) + 1)
+        let upperCaseWordArray = wordArray.map((word) => {return word[0].toUpperCase() + word.slice(1)})
+        let upperCaseWord = ""
+        upperCaseWordArray.forEach(word => {
+            upperCaseWord += word + " "
+        })
+        return upperCaseWord.trim()
+    })
+}
+
 // code
 
 const tags = [
 ]
 
-onEvent("rei.group", (rei) => {
-    tags.forEach(tag => {
-        rei.groupItemsByTag(`${tag.split(":")[0]}:rei_groups/${tag.split(":")[1]}`, tag.replace(/^(\w)/, c => c.toUpperCase()).replace(/_(\w)/g, (_, c) => " " + c.toUpperCase()).slice(tag.search(/[:]/) + 1), tag), tag})
+const blockNames = blockIdToBlockName(tags)
+
+onEvent("rei.group", (event) => {
+    for (let i = 0; i < tags.length; i++) {
+        let tagNoModId = tags[i].slice(tags[i].search(/[:]/) + 1)
+        event.groupItemsByTag(`chipped:rei_groups/${tagNoModId}`, blockNames[i], tags[i]);
+    }
 })
