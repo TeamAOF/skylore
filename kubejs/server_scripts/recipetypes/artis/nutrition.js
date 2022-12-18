35443a566ebb2d79cebb3f379f7eb1a912d11e57
Monster Zero <dinanj@gmail.com>
2022-12-18 13:31:06 -0500

    artis recipes

---
+++ kubejs/server_scripts/recipetypes/artis/nutrition.js
@@ -0,0 +1,7 @@
+
+
+onEvent('recipes', (event) => {
+	
+  event.shapeless('artis:chef_table', ['minecraft:crafting_table', 'minecraft:cooked_beef'])
+
+});
---
+++ kubejs/server_scripts/recipetypes/artis/science.js
@@ -0,0 +1,5 @@
+onEvent('recipes', (event) => {
+	
+  event.shapeless('artis:engineering_panel', ['minecraft:crafting_table', 'minecraft:oak_sapling'])
+
+});
