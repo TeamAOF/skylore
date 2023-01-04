8b48756fac12abf4d81bf8733f1287a9b3c778b1
_Robertas <robertasj8@gmail.com>
2023-01-04 10:50:17 +0100

    changed recipe to be compatible with vanilla tool removal

---
+++ kubejs/server_scripts/recipetypes/farmersdelight/shaped.js
@@ -0,0 +1,24 @@
+onEvent('recipes', (event) => {
+
+    event.shaped("farmersdelight:cooking_pot",
+    [
+        "bSb",
+        "iWi",
+        "iii"
+    ],
+    {
+        "S": {
+            "item": "tconstruct:mattock"
+        },
+        "W": {
+            "item": "minecraft:water_bucket"
+        },
+        "b": {
+            "item": "minecraft:brick"
+        },
+        "i": {
+            "tag": "c:iron_ingots"
+        }
+    }).id("farmersdelight:cooking_pot")
+
+})
