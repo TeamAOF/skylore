0544361776980df277d219520840d06a81896824
Monster Zero <dinanj@gmail.com>
2023-01-06 11:30:49 -0500

    mill more

--- kubejs/server_scripts/recipetypes/create/recipes/milling.js
+++ kubejs/server_scripts/recipetypes/create/recipes/milling.js
@@ -13,7 +13,45 @@ onEvent('recipes', (event) => {
           "item": "minecraft:cobbled_deepslate"
         }
       ],
-      "processingTime": 350
+      "processingTime": 1050
+    }
+  )
+})
+onEvent('recipes', (event) => {
+
+  event.custom(
+    {
+      "type": "create:milling",
+      "ingredients": [
+        {
+          "item": "minecraft:gravel"
+        }
+      ],
+      "results": [
+        {
+          "item": "minecraft:sand"
+        }
+      ],
+      "processingTime": 1050
+    }
+  )
+})
+onEvent('recipes', (event) => {
+
+  event.custom(
+    {
+      "type": "create:milling",
+      "ingredients": [
+        {
+          "item": "fabricaeexnihilo:silt"
+        }
+      ],
+      "results": [
+        {
+          "item": "fabricaeexnihilo:dust"
+        }
+      ],
+      "processingTime": 1050
     }
   )
 })
