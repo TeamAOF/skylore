f2b10e84c588260af57daa51037b5a908d81217d
Monster Zero <dinanj@gmail.com>
2023-01-06 12:49:42 -0500

    create plates

--- kubejs/server_scripts/recipetypes/create/recipes/pressing.js
+++ kubejs/server_scripts/recipetypes/create/recipes/pressing.js
@@ -42,3 +42,39 @@ onEvent('recipes', (event) => {
     }
   )
 })
+onEvent('recipes', (event) => {
+
+    const plates = [
+        'bronze',
+        'tin',
+        'lead',
+        'silver',
+        'nickel',
+        'tungsten',
+        'titanium',
+        'chromium',
+        'invar',
+        'nickel',
+        'platinum',
+        'stainless_steel',
+        'steel'
+
+    ];
+
+    plates.forEach((item) => {
+        event.custom({
+            "type": "create:pressing",
+            "ingredients": [
+                {
+                    "tag": "c:" + item + "_ingots"
+                }
+            ],
+            "results": [
+                {
+                    "item": "modern_industrialization:" + item + "_plate",
+                    "count": 1
+                },
+            ],
+        })
+    });
+})
