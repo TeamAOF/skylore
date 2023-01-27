65d1bd31e3b20d46d41c9d9659163981d1e694a9
AK9 <anthonkrisat@hotmail.com>
2023-01-27 15:34:00 +0100

    tinkers part 3: melting & casting

---
+++ kubejs/server_scripts/recipetypes/tinkers/melting.js
@@ -0,0 +1,95 @@
+onEvent('recipes', (event) => {
+
+  const materials = [
+    'tin',
+    'lead',
+    'silver',
+    'nickel',
+    'tungsten',
+    'mozanite',
+    'uranium',
+    'antimony',
+    'iridium',
+    'steel',
+  ];
+
+  const raw_materials = [
+    'tin',
+    'lead',
+    'silver',
+    'nickel',
+    'tungsten',
+    'uranium',
+    'antimony',
+    'iridium'
+  ];
+
+  materials.forEach((item) => {
+    event.custom({
+      "type": "tconstruct:melting",
+      "ingredient": {
+        "tag": "c:" + item + "_ores"
+      },
+      "result": {
+        "fluid": "kubejs:molten_" + item,
+        "amount": 18000
+      },
+      "temperature": 500,
+      "time": 50
+    })
+
+    event.custom({
+        "type": "tconstruct:melting",
+        "ingredient": {
+          "tag": "c:" + item + "_dusts"
+        },
+        "result": {
+          "fluid": "kubejs:molten_" + item,
+          "amount": 9000
+        },
+        "temperature": 500,
+        "time": 50
+      })
+
+      event.custom({
+        "type": "tconstruct:melting",
+        "ingredient": {
+          "tag": "c:" + item + "_ingots"
+        },
+        "result": {
+          "fluid": "kubejs:molten_" + item,
+          "amount": 9000
+        },
+        "temperature": 500,
+        "time": 50
+      })
+
+      event.custom({
+        "type": "tconstruct:melting",
+        "ingredient": {
+          "tag": "c:" + item + "_plates"
+        },
+        "result": {
+          "fluid": "kubejs:molten_" + item,
+          "amount": 9000
+        },
+        "temperature": 500,
+        "time": 50
+      })
+  })
+
+  raw_materials.forEach((item) => {
+    event.custom({
+      "type": "tconstruct:melting",
+      "ingredient": {
+        "tag": "c:raw_" + item + "_ores"
+      },
+      "result": {
+        "fluid": "kubejs:molten_" + item,
+        "amount": 9000
+      },
+      "temperature": 500,
+      "time": 50
+    })
+  })
+})
--- kubejs/startup_scripts/tinkers/fluids.js
+++ kubejs/startup_scripts/tinkers/fluids.js
@@ -6,6 +6,7 @@ onEvent('fluid.registry', event => {
     .bucketColor(0xC9C3E5)
     .displayName('Molten Tin')
     .luminosity(8)
+    .noBucket()
 
   // Lead
   event.create('molten_lead')
@@ -13,6 +14,7 @@ onEvent('fluid.registry', event => {
     .bucketColor(0x4B547E)
     .displayName('Molten Lead')
     .luminosity(8)
+    .noBucket()
 
   // Silver
   event.create('molten_silver')
@@ -20,6 +22,7 @@ onEvent('fluid.registry', event => {
     .bucketColor(0xACCBE7)
     .displayName('Molten Silver')
     .luminosity(8)
+    .noBucket()
 
   // Nickel
   event.create('molten_nickel')
@@ -27,6 +30,7 @@ onEvent('fluid.registry', event => {
     .bucketColor(0xCECEA5)
     .displayName('Molten Nickel')
     .luminosity(8)
+    .noBucket()
 
   // Tungsten
   event.create('molten_tungsten')
@@ -34,6 +38,7 @@ onEvent('fluid.registry', event => {
     .bucketColor(0x665585)
     .displayName('Molten Tungsten')
     .luminosity(8)
+    .noBucket()
 
   // Mozanite
   event.create('molten_mozanite')
@@ -41,6 +46,7 @@ onEvent('fluid.registry', event => {
     .bucketColor(0x8D2185)
     .displayName('Molten Mozanite')
     .luminosity(8)
+    .noBucket()
 
   // Uranium
   event.create('molten_uranium')
@@ -48,6 +54,7 @@ onEvent('fluid.registry', event => {
     .bucketColor(0x93C930)
     .displayName('Molten Uranium')
     .luminosity(8)
+    .noBucket()
 
   // Antimony
   event.create('molten_antimony')
@@ -55,6 +62,7 @@ onEvent('fluid.registry', event => {
     .bucketColor(0xA5A5B4)
     .displayName('Molten Antimony')
     .luminosity(8)
+    .noBucket()
 
   // Iridium
   event.create('molten_iridium')
@@ -62,6 +70,15 @@ onEvent('fluid.registry', event => {
     .bucketColor(0xE5E5E5)
     .displayName('Molten Iridium')
     .luminosity(8)
+    .noBucket()
+
+  // Steel
+  event.create('molten_steel')
+    .thickTexture(0x343334)
+    .bucketColor(0x343334)
+    .displayName('Molten Steel')
+    .luminosity(8)
+    .noBucket()
 
   // Alloys (TO BE DONE)
 })
