9d82abad8ab431eae54ca1c52b32c4f599395477
AK9 <anthonkrisat@hotmail.com>
2023-01-27 14:18:35 +0100

    tinkers part 2: names and colors

--- kubejs/startup_scripts/tinkers/fluids.js
+++ kubejs/startup_scripts/tinkers/fluids.js
@@ -1,30 +1,67 @@
 onEvent('fluid.registry', event => {
 
-  const mi_fluids = [
-    // Tin
-    0x747BAD,
-    // Lead
-    0x4B547E,
-    // Silver
-    0xACCBE7,
-    // Nickel
-    0xCECEA5,
-    // Tungsten
-    0x665585,
-    // Mozanite
-    0x8D2185,
-    // Uranium
-    0x93C930,
-    // Antimony
-    0xA5A5B4,
-    // Iridium
-    0xE5E5E5,
-];
+  // Tin
+  event.create('molten_tin')
+    .thickTexture(0xC9C3E5)
+    .bucketColor(0xC9C3E5)
+    .displayName('Molten Tin')
+    .luminosity(8)
 
-mi_fluids.forEach((item) => {
-  event.create('thick_fluid')
-    .thickTexture(item)
-    .bucketColor(item)
-  	.luminosity(8)
- })
+  // Lead
+  event.create('molten_lead')
+    .thickTexture(0x4B547E)
+    .bucketColor(0x4B547E)
+    .displayName('Molten Lead')
+    .luminosity(8)
+
+  // Silver
+  event.create('molten_silver')
+    .thickTexture(0xACCBE7)
+    .bucketColor(0xACCBE7)
+    .displayName('Molten Silver')
+    .luminosity(8)
+
+  // Nickel
+  event.create('molten_nickel')
+    .thickTexture(0xCECEA5)
+    .bucketColor(0xCECEA5)
+    .displayName('Molten Nickel')
+    .luminosity(8)
+
+  // Tungsten
+  event.create('molten_tungsten')
+    .thickTexture(0x665585)
+    .bucketColor(0x665585)
+    .displayName('Molten Tungsten')
+    .luminosity(8)
+
+  // Mozanite
+  event.create('molten_mozanite')
+    .thickTexture(0x8D2185)
+    .bucketColor(0x8D2185)
+    .displayName('Molten Mozanite')
+    .luminosity(8)
+
+  // Uranium
+  event.create('molten_uranium')
+    .thickTexture(0x93C930)
+    .bucketColor(0x93C930)
+    .displayName('Molten Uranium')
+    .luminosity(8)
+
+  // Antimony
+  event.create('molten_antimony')
+    .thickTexture(0xA5A5B4)
+    .bucketColor(0xA5A5B4)
+    .displayName('Molten Antimony')
+    .luminosity(8)
+
+  // Iridium
+  event.create('molten_iridium')
+    .thickTexture(0xE5E5E5)
+    .bucketColor(0xE5E5E5)
+    .displayName('Molten Iridium')
+    .luminosity(8)
+
+  // Alloys (TO BE DONE)
 })
