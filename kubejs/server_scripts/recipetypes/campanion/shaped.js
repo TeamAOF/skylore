d87ce6ab977a4c66e18c3b234cb647cf52b01c1b
_Robertas <robertasj8@gmail.com>
2023-01-28 15:20:25 +0100

    fix #68

---
+++ kubejs/server_scripts/recipetypes/campanion/shaped.js
@@ -0,0 +1,15 @@
+onEvent('recipes', (event) => {
+    event.shaped("campanion:grappling_hook",
+    [
+        " I ",
+        "PRI",
+        "SI "
+    ],
+    {
+        "I": "#c:iron_ingots",
+        "P": "#minecraft:planks",
+        "R": "farmersdelight:rope",
+        "S": "minecraft:stick"
+    }).id("campanion:grappling_hook")
+
+})
